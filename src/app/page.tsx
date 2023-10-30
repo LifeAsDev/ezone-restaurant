eb6ef89662e4bffebf63dd8b5dfee485c02f4d8c
LifeAsDev <angelocraft551@gmail.com>
2023-10-30 19:54:41 -0400

    pay ui added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,8 @@ export default function Home() {
   const [reorderedProducts, setReorderedProducts] = useState<any[]>([]);
   const [cartItems, setCartItems] = useState<any[]>([]);
   const [checkPay, setCheckingPay] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("pix");
+  const [loadingPay, setLoadingPay] = useState(false);
   const addToCart = (item: string) => {
     if (logged) {
       const itemIndex = cartItems.findIndex(
@@ -88,6 +90,17 @@ export default function Home() {
       cartList.classList.toggle("show-cart-list");
     }
   }
+  function toggleClass(id: string, classToggle: string, method: string): void {
+    const cartList = document.getElementById(id);
+
+    if (cartList) {
+      if (method === "add") {
+        cartList.classList.add(classToggle);
+      } else if (method === "remove") {
+        cartList.classList.remove(classToggle);
+      }
+    }
+  }
   const totalPrice: number =
     Math.round(
       (cartItems.reduce(
@@ -98,14 +111,305 @@ export default function Home() {
         Number.EPSILON) *
         100
     ) / 100;
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
+  };
   return (
     <main>
+      <div
+        onClick={() => toggleClass("complete-order", "none", "add")}
+        id="complete-order"
+        className=" cursor-pointer order-complete fixed text-[.95rem] z-[4] text-[#808080] rounded-lg top-4 bg-white p-4 font-[500] flex flex-row hover:bg-[#deffdb]"
+      >
+        <div className="bg-green-400 rounded-[50%] flex items-center justify-center h-[2rem] min-w-[2rem] mr-4 my-auto">
+          <Image src={"/img/ready.png"} height={18} width={18} alt="logo" />
+        </div>
+        <p className="w-[14.5rem]">
+          Order complete successfuly with payment. Thank you for your patronage.
+        </p>
+        <div className="relative ml-4">
+          <div className="absolute font-[600] text-[1.5rem] right-[-.4rem] top-[-.85rem]">
+            x
+          </div>
+        </div>
+      </div>
       <div
         id="cart-list"
         className="opacity-0 invisible bg-white fixed h-[100vh] w-[25%] z-[3] right-[-20%] p-6 px-0"
       >
         {checkPay ? (
-          <div></div>
+          <>
+            <div className="flex justify-between mx-6 mb-6">
+              <svg
+                onClick={() => setCheckingPay(false)}
+                className="cursor-pointer"
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                  <g
+                    id="Arrow"
+                    transform="translate(-480.000000, 0.000000)"
+                    fillRule="nonzero"
+                  >
+                    <g
+                      id="back_2_line"
+                      transform="translate(480.000000, 0.000000)"
+                    >
+                      <path
+                        d="M24,0 L24,24 L0,24 L0,0 L24,0 Z M12.5934901,23.257841 L12.5819402,23.2595131 L12.5108777,23.2950439 L12.4918791,23.2987469 L12.4918791,23.2987469 L12.4767152,23.2950439 L12.4056548,23.2595131 C12.3958229,23.2563662 12.3870493,23.2590235 12.3821421,23.2649074 L12.3780323,23.275831 L12.360941,23.7031097 L12.3658947,23.7234994 L12.3769048,23.7357139 L12.4804777,23.8096931 L12.4953491,23.8136134 L12.4953491,23.8136134 L12.5071152,23.8096931 L12.6106902,23.7357139 L12.6232938,23.7196733 L12.6232938,23.7196733 L12.6266527,23.7031097 L12.609561,23.275831 C12.6075724,23.2657013 12.6010112,23.2592993 12.5934901,23.257841 L12.5934901,23.257841 Z M12.8583906,23.1452862 L12.8445485,23.1473072 L12.6598443,23.2396597 L12.6498822,23.2499052 L12.6498822,23.2499052 L12.6471943,23.2611114 L12.6650943,23.6906389 L12.6699349,23.7034178 L12.6699349,23.7034178 L12.678386,23.7104931 L12.8793402,23.8032389 C12.8914285,23.8068999 12.9022333,23.8029875 12.9078286,23.7952264 L12.9118235,23.7811639 L12.8776777,23.1665331 C12.8752882,23.1545897 12.8674102,23.1470016 12.8583906,23.1452862 L12.8583906,23.1452862 Z M12.1430473,23.1473072 C12.1332178,23.1423925 12.1221763,23.1452606 12.1156365,23.1525954 L12.1099173,23.1665331 L12.0757714,23.7811639 C12.0751323,23.7926639 12.0828099,23.8018602 12.0926481,23.8045676 L12.108256,23.8032389 L12.3092106,23.7104931 L12.3186497,23.7024347 L12.3186497,23.7024347 L12.3225043,23.6906389 L12.340401,23.2611114 L12.337245,23.2485176 L12.337245,23.2485176 L12.3277531,23.2396597 L12.1430473,23.1473072 Z"
+                        id="MingCute"
+                        fillRule="nonzero"
+                      ></path>
+                      <path
+                        d="M6.04599,11.6767 C7.35323,9.47493 9.75524,8 12.5,8 C16.6421,8 20,11.3579 20,15.5 C20,16.0523 20.4477,16.5 21,16.5 C21.5523,16.5 22,16.0523 22,15.5 C22,10.2533 17.7467,6 12.5,6 C9.31864,6 6.50386,7.56337 4.78,9.96279 L4.24303,6.91751 C4.14713,6.37361 3.62847,6.01044 3.08458,6.10635 C2.54068,6.20225 2.17751,6.72091 2.27342,7.2648 L3.31531,13.1736 C3.36136,13.4348 3.50928,13.667 3.72654,13.8192 C4.0104,14.0179 4.38776,14.0542 4.70227,13.9445 L10.3826,12.9429 C10.9265,12.847 11.2897,12.3284 11.1938,11.7845 C11.0979,11.2406 10.5792,10.8774 10.0353,10.9733 L6.04599,11.6767 Z"
+                        id="路径"
+                        fill="#09244B"
+                      ></path>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+              <div className="flex justify-between items-center gap-2 cursor-pointer">
+                <p className="font-[500]">Secured Payment</p>
+              </div>
+              <div
+                className="flex items-center justify-center gap-1"
+                title="Secured"
+              >
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 16 16"
+                  className="text-xl cursor-pointer fill-red-500"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"></path>
+                  <path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415z"></path>
+                </svg>
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 24 24"
+                  className="text-xl cursor-pointer fill-red-500"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g>
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path
+                      fillRule="nonzero"
+                      d="M11 2l7.298 2.28a1 1 0 0 1 .702.955V7h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1l-3.22.001c-.387.51-.857.96-1.4 1.33L11 22l-5.38-3.668A6 6 0 0 1 3 13.374V5.235a1 1 0 0 1 .702-.954L11 2zm0 2.094L5 5.97v7.404a4 4 0 0 0 1.558 3.169l.189.136L11 19.58 14.782 17H10a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h7V5.97l-6-1.876zM11 12v3h9v-3h-9zm0-2h9V9h-9v1z"
+                    ></path>
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div className="bg-[#292c28] h-full rounded-t-[2rem] flex flex-col justify-between">
+              <div className="flex flex-col px-4 ">
+                <div className="flex flex-row justify-between py-6 ">
+                  <label
+                    htmlFor="pix"
+                    className={`cursor-pointer w-[45%] flex flex-row items-center gap-4 rounded-full px-2 py-[.2rem] ${
+                      selectedOption === "pix" ? "bg-[#e3e3e3]" : null
+                    }`}
+                  >
+                    <input
+                      disabled={loadingPay}
+                      className="h-5 w-5"
+                      type="radio"
+                      id="pix"
+                      name="pay method"
+                      value="pix"
+                      checked={selectedOption === "pix"}
+                      onChange={handleOptionChange}
+                    />
+                    <div className="flex items-center justify-center w-[50%]">
+                      <Image
+                        src={"/img/pix.webp"}
+                        height={32}
+                        width={40}
+                        alt="logo"
+                      />
+                    </div>
+                  </label>
+                  <label
+                    htmlFor="card"
+                    className={`cursor-pointer w-[45%] flex flex-row items-center gap-2 rounded-full p-2 ${
+                      selectedOption === "card" ? "bg-[#e3e3e3]" : null
+                    }`}
+                  >
+                    <input
+                      disabled={loadingPay}
+                      className="h-5 w-5"
+                      type="radio"
+                      id="card"
+                      name="pay method"
+                      value="card"
+                      checked={selectedOption === "card"}
+                      onChange={handleOptionChange}
+                    />
+                    <Image
+                      src={"/img/visa.webp"}
+                      height={30}
+                      width={40}
+                      alt="logo"
+                    />
+                    <Image
+                      src={"/img/mastercard.webp"}
+                      height={30}
+                      width={40}
+                      alt="logo"
+                    />
+                  </label>
+                </div>
+                {selectedOption === "card" ? (
+                  <>
+                    <div className="w-full p-1 px-2 rounded-lg flex flex-col">
+                      <div className="w-full flex flex-col mb-2">
+                        <label
+                          htmlFor="name"
+                          className="font-bold text-sm mb-1 text-gray-100"
+                        >
+                          Name on Card
+                        </label>
+                        <input
+                          type="text"
+                          id="name"
+                          className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-[#323631] transition-colors"
+                          placeholder="Enter your name"
+                          autoComplete="off"
+                        />
+                      </div>
+                      <div className="w-full flex flex-col mb-2">
+                        <label
+                          htmlFor="number"
+                          className="font-bold text-sm mb-1 text-gray-100"
+                        >
+                          Card Number
+                        </label>
+                        <input
+                          type="text"
+                          id="number"
+                          className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-[#323631] transition-colors"
+                          placeholder="Enter your number"
+                          autoComplete="off"
+                        />
+                      </div>
+                      <div className="w-full flex justify-between gap-1 mb-2">
+                        <div className="flex flex-col ">
+                          <label
+                            htmlFor="number"
+                            className="font-bold text-sm mb-1 text-gray-100"
+                          >
+                            Expiry Date
+                          </label>
+                          <input
+                            type="text"
+                            id="text"
+                            className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-[#323631]"
+                            placeholder="MM/YY"
+                            autoComplete="off"
+                          />
+                        </div>
+                        <div className="flex flex-col ">
+                          <label
+                            htmlFor="number"
+                            className="font-bold text-sm mb-1 text-gray-100"
+                          >
+                            CVV
+                          </label>
+                          <input
+                            type="text"
+                            id="password"
+                            className="w-full px-3 py-2 mb-1 border-2 text-white border-gray-500 rounded-md focus:outline-none focus:border-orange-500 focus:text-orange-500 bg-[#323631] transition-colors"
+                            placeholder="CVV"
+                            autoComplete="off"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : null}
+                <p className="mt-4 w-full text-gray-300 font-[600] text-center">
+                  Amount Due:{" "}
+                  <span className="font-bold text-white">${totalPrice}</span>
+                </p>
+                <button
+                  disabled={loadingPay}
+                  onClick={() => {
+                    setLoadingPay(true);
+
+                    setTimeout(function () {
+                      setCheckingPay(false);
+                      toggleInvisibleClass();
+                      toggleClass("complete-order", "none", "remove");
+                      setLoadingPay(false);
+                      setCartItems([]);
+                    }, 1000);
+                  }}
+                  className="w-full p-2 rounded-full bg-gradient-to-tr from-green-400 to-green-600 text-gray-50 text-lg my-2 hover:shadow-lg font-[600]
+                flex flex-row items-center justify-center gap-2"
+                >
+                  {loadingPay ? (
+                    <div className="loader"></div>
+                  ) : (
+                    <>
+                      <svg
+                        width="1.4rem"
+                        height="1.4rem"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288"
+                          stroke="white"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      PAY NOW
+                    </>
+                  )}
+                </button>
+              </div>
+              <div className="bg-[#323631] grow px-6 rounded-t-[2rem] flex flex-col py-8 gap-5 mt-8 ">
+                <div className="flex flex-row w-full text-white items-center justify-center font-[600] gap-2">
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 16 16"
+                    className="text-xl cursor-pointer fill-red-500"
+                    height="1.5rem"
+                    width="1.5rem"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M5.338 1.59a61.44 61.44 0 0 0-2.837.856.481.481 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.725 10.725 0 0 0 2.287 2.233c.346.244.652.42.893.533.12.057.218.095.293.118a.55.55 0 0 0 .101.025.615.615 0 0 0 .1-.025c.076-.023.174-.061.294-.118.24-.113.547-.29.893-.533a10.726 10.726 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.775 11.775 0 0 1-2.517 2.453 7.159 7.159 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7.158 7.158 0 0 1-1.048-.625 11.777 11.777 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 62.456 62.456 0 0 1 5.072.56z"></path>
+                    <path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415z"></path>
+                  </svg>
+                  <p>
+                    Secured by
+                    <span className="text-green-500 text-[.95rem] font-[800]">
+                      {" "}
+                      ZONESTACK
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </>
         ) : (
           <>
             <div className="flex justify-between mx-6 mb-6">
